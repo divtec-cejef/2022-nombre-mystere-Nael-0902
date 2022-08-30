757dd80a4293b5bbccaed25ba7d9a0e3581163d5
Nael-0902 <94693006+Nael-0902@users.noreply.github.com>
2022-08-30 16:30:05 +0200

    Finalisation

--- js/deviner-nombre.js
+++ js/deviner-nombre.js
@@ -21,24 +21,21 @@
     function tireNombre(min, max) {
         return Math.floor(Math.random() * (max - min) + min);
     }
-
     let nbMystere = tireNombre(MIN, MAX);
     console.log(nbMystere);
-
-    let lastResult = null;
     let nbCoups = 0;
     do {
-        lastResult = prompt('Entrez un nombre entre '+MIN+' et '+MAX);
+        let lastResult = prompt('Entrez un nombre entre '+MIN+' et '+MAX);
         if (isNaN(lastResult) || lastResult < MIN || lastResult > MAX) {
             alert('Saisie invalide');
         }
         else {
             let message = lastResult < nbMystere ? 'C\'est plus' : 'C\'est moins';
             ++nbCoups;
-            alert(message);
-            if (lastResult === nbMystere) {
+            if (lastResult == nbMystere) {
                 break;
             }
+            alert(message);
         }
     } while (true);
     alert('Bravo, tu as réussi en '+nbCoups+ ' coups');
